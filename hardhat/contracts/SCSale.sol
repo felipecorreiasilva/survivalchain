cb173eeed424ae44a0a63e31bf5baf90f05e2bcd
felipecorreiasilva <felipecorreiasilva@outlook.com>
2024-10-10 02:33:05 -0300

    tokensale corrections

--- hardhat/contracts/SCSale.sol
+++ hardhat/contracts/SCSale.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.27;
 
 import "./SCToken.sol";
-import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { UD60x18, ud } from "@prb/math/src/UD60x18.sol";
 import { SD59x18, sd } from "@prb/math/src/SD59x18.sol";
 
 contract SCSale {
+    
     address payable public admin;
-    address payable private ethFunds;
     SCToken public token;
     uint256 public tokensSold;
-    SD59x18 private tokenPriceUSD;
+    uint256 public tokenPriceUSD;
     AggregatorV3Interface internal priceFeed;
 
     uint256 public transactionCount;
@@ -27,10 +28,9 @@ contract SCSale {
     constructor(SCToken _token) {
         
         priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        tokenPriceUSD = sd(9); // 0.09 USD (0.00) 3 DIGITS
+        tokenPriceUSD = 9; // 0.09 USD (0.00) 3 DIGITS
         token = _token;
         admin = payable(msg.sender);
-        ethFunds = payable(admin);
 
     }
     
@@ -45,25 +45,37 @@ contract SCSale {
         return (price/1e6);
     }
 
-    function sCTokenPriceInETH() public view returns(SD59x18) {
-        int _ethPrice = getETHPrice();
-        SD59x18 ethPrice = sd(_ethPrice);
-        return tokenPriceUSD.div(ethPrice);
+    function calculateTokenPrice(uint256 _numberOfTokens) public view returns (UD60x18) {
+        // Fetch the latest ETH price in USD
+        (, int256 ethPrice, , ,) = priceFeed.latestRoundData();
+        require(ethPrice > 0, "Invalid price data");
+
+        // Calculate required Ether using PRBMath for fixed-point arithmetic
+        UD60x18 ethPriceInUSD = ud(uint256(ethPrice/1e6));
+        UD60x18 totalCostInUSD = ud(tokenPriceUSD*_numberOfTokens); // Total cost in USD
+        UD60x18 requiredEther = (totalCostInUSD) / ethPriceInUSD; // Convert to Ether
+
+        return requiredEther;
     }
 
     function buyToken(uint256 _amount) public payable {
-        SD59x18 sCTokenPriceETH = sCTokenPriceInETH();
-        SD59x18 msgValue = sd(int(msg.value));
-        SD59x18 _amountSd = sd(int(_amount));
+        
+        UD60x18 _calculateTokenPrice = calculateTokenPrice(_amount);
+        UD60x18 msgValue = ud(uint(msg.value));
+
         // Check that the buyer sends the enough ETH
-        require(msgValue >= sCTokenPriceETH * _amountSd);
+        require(_amount > 0, "Does not accept 0 in amount");
+        require(msg.value > 0, "Send ETH to buy some tokens");
+        require(msgValue >= _calculateTokenPrice, "There is not enough ether to buy tokens");
+
         // check that the sale contract provides the enough ETH to make this transaction
-        require(token.balanceOf(address(this)) >= _amount);
+        require(token.balanceOf(address(this)) >= _amount, "Insufficient tokens in contract");
+        
         // Make the transaction inside of the require
         // transfer returns a boolean value.
         require(token.transfer(msg.sender, _amount));
         // Transfer the ETH of the buyer to us
-        ethFunds.transfer(msg.value);
+        admin.transfer(msg.value);
         // Increase the amount of tokens sold
         tokensSold += _amount;
         // Increase the amount of transactions
